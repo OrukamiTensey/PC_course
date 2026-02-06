0ec5108a2c404b4398ffa37de332e96e0227eeef
orukamitensey@gmail.com <orukamitensey@gmail.com>
2026-02-06 14:50:52 +0200

    Sequential solve and matrix generator added

--- task1/main.cpp
+++ task1/main.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-// 1. Механізм для заміру часу виконання
+// 1. Time analysis mechanism
 class ScopedTimer {
 public:
     ScopedTimer(const string& name) : name_(name), start_(chrono::high_resolution_clock::now()) {}
@@ -23,3 +23,29 @@ private:
     string name_;
     chrono::time_point<chrono::high_resolution_clock> start_;
 };
+
+// Matrix generation function
+void fillMatrix(vector<vector<int>>& matrix, int rows, int cols) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distrib(1, 100);
+
+    for (int i = 0; i < rows; ++i) {
+        matrix[i].resize(cols);
+        for (int j = 0; j < cols; ++j) {
+            matrix[i][j] = distrib(gen);
+        }
+    }
+}
+
+// 2. Easy solution: C = A + k*B
+void solveSequential(const vector<vector<int>>& A,
+    const vector<vector<int>>& B,
+    vector<vector<int>>& C,
+    int rows, int cols, int k) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            C[i][j] = A[i][j] + k * B[i][j];
+        }
+    }
+}
