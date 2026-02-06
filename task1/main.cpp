5e868704468aa64773ebdad9028755e2ce43ee70
orukamitensey@gmail.com <orukamitensey@gmail.com>
2026-02-06 14:47:52 +0200

    ScopedTimer class added

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,20 +1,25 @@
-// task1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <functional>
+#include <iomanip>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+using namespace std;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// 1. Механізм для заміру часу виконання
+class ScopedTimer {
+public:
+    ScopedTimer(const string& name) : name_(name), start_(chrono::high_resolution_clock::now()) {}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    ~ScopedTimer() {
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(end - start_).count();
+        cout << "Time derived for [" << name_ << "]: " << duration << " ms" << endl;
+    }
+
+private:
+    string name_;
+    chrono::time_point<chrono::high_resolution_clock> start_;
+};
