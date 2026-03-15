24cb243e42ec3cf49f37e0dc657b228cf5b8e745
orukamitensey@gmail.com <orukamitensey@gmail.com>
2026-03-15 22:07:57 +0200

    Add ScopedTimer for perfomance analysis

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,7 +1,25 @@
-
+#include <vector>
+#include <chrono>
 #include <iostream>
+
+using namespace std 
+
+class ScopedTimer
+{
+public:
+    ScopedTimer(long long* outTime) : outTime_(outTime), start_(chrono::high_resolution_clock::now()) {}
+    ~ScopedTimer()
+    {
+        auto end = chrono::high_resolution_clock::now();
+        *outTime_ = chrono::duration_cast<chrono::milliseconds>(end - start_).count();
+    }
+private:
+    long long* outTime_;
+    chrono::time_point<chrono::high_resolution_clock> start_;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Parallel Computing: Task 2 (Variant 17)" << endl;
+    return 0;
 }
